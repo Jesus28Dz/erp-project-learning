a38cd054febcb9f7955ead18222cd3e324431f1e
Jesus28Dz <159570619+Jesus28Dz@users.noreply.github.com>
2026-07-06 15:24:51 -0600

    More tables were added to the database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 ----- catalog tables -----
-
 CREATE TABLE companies(
   id SERIAL PRIMARY KEY,
   company_name VARCHAR(30)
@@ -18,4 +17,39 @@ CREATE TABLE modes(
 CREATE TABLE amounts(
   id SERIAL PRIMARY KEY,
   fixed_amount DECIMAL(10,2)
+);
+
+CREATE TABLE roles(
+  id SERIAL PRIMARY KEY,
+  role_name VARCHAR(20),
+  privilege INT  
+);
+
+----- clients & users -----
+CREATE TABLE clients(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  reference VARCHAR(15),
+  address TEXT,
+  phone1 VARCHAR(15),
+  phone2 VARCHAR(15),
+  email VARCHAR(60)
+);
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  username VARCHAR(15),
+  password VARCHAR(255),
+  address TEXT,
+  phone VARCHAR(15),
+  email VARCHAR(60),
+  role_id INT REFERENCES roles(id)
+);
+
+----- tables with foreign keys -----
+CREATE TABLE company_platform(
+  id SERIAL PRIMARY KEY,
+  company_id INT REFERENCES companies(id),
+  platform_id INT REFERENCES platforms(id)
 );
