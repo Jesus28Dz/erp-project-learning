565bb1eee3653039bbdd0631726a7444ec887ede
Jesus28Dz <159570619+Jesus28Dz@users.noreply.github.com>
2026-07-06 15:01:32 -0600

    Create schema.sql
    The catalog tables were added.

---
+++ schema.sql
@@ -0,0 +1,21 @@
+----- catalog tables -----
+
+CREATE TABLE companies(
+  id SERIAL PRIMARY KEY,
+  company_name VARCHAR(30)
+);
+
+CREATE TABLE platforms(
+  id SERIAL PRIMARY KEY,
+  platform_name VARCHAR(30)
+);
+
+CREATE TABLE modes(
+  id SERIAL PRIMARY KEY,
+  mode_name VARCHAR(30)
+);
+
+CREATE TABLE amount(
+  id SERIAL PRIMARY KEY,
+  fixed_amount DECIMAL(10,2)
+);
